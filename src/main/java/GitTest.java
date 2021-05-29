87f1d79640dc26a8c38176984c7bf7c1fdae07c0
yangxianchang <1379254968@qq.com>
2021-05-29 16:24:07 +0800

    hot-fix commit2

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git1");
         System.out.println("hot-fix");
+        System.out.println("hot-fix1");
     }
 }
