5232f871eab0096e9fe998308b99ea19c4edee12
yangxianchang <1379254968@qq.com>
2021-05-29 16:50:02 +0800

    push

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("hot-fix1");
         System.out.println("hot-fix2");
+        System.out.println("push");
     }
 }
